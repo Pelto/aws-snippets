d67aac448cc59ee81597f86e25826443c39ff2bf
Johannes Pelto-Piri <johannes.peltopiri@jayway.com>
2018-09-20 15:10:40 +0200

    scripts: refactor delete-versioned-bucket.sh

--- scripts/delete-versioned-bucket/delete-versioned-bucket.sh
+++ scripts/delete-versioned-bucket/delete-versioned-bucket.sh
@@ -32,28 +32,30 @@ markers=`aws s3api list-object-versions --bucket $bucket |jq '.DeleteMarkers'`
 let count=`echo $versions |jq 'length'`-1
 
 if [ $count -gt -1 ]; then
-        echo "removing files"
-        for i in $(seq 0 $count); do
-                key=`echo $versions | jq .[$i].Key |sed -e 's/\"//g'`
-                versionId=`echo $versions | jq .[$i].VersionId |sed -e 's/\"//g'`
-                cmd="aws s3api delete-object --bucket $bucket --key $key --version-id $versionId"
-                echo $cmd
-                $cmd
-        done
+    echo "removing files"
+    for i in $(seq 0 $count); do
+        key=`echo $versions | jq .[$i].Key |sed -e 's/\"//g'`
+        versionId=`echo $versions | jq .[$i].VersionId |sed -e 's/\"//g'`
+        aws s3api delete-object \
+            --bucket $bucket \
+            --key $key \
+            --version-id $versionId
+    done
 fi
 
 let count=`echo $markers |jq 'length'`-1
 
 if [ $count -gt -1 ]; then
-        echo "removing delete markers"
+    echo "removing delete markers"
 
-        for i in $(seq 0 $count); do
-                key=`echo $markers | jq .[$i].Key |sed -e 's/\"//g'`
-                versionId=`echo $markers | jq .[$i].VersionId |sed -e 's/\"//g'`
-                cmd="aws s3api delete-object --bucket $bucket --key $key --version-id $versionId"
-                echo $cmd
-                $cmd
-        done
+    for i in $(seq 0 $count); do
+        key=`echo $markers | jq .[$i].Key |sed -e 's/\"//g'`
+        versionId=`echo $markers | jq .[$i].VersionId |sed -e 's/\"//g'`
+        aws s3api delete-object \
+            --bucket $bucket \
+            --key $key \
+            --version-id $versionId
+    done
 fi
 
 aws s3 rb --force s3://$bucket
